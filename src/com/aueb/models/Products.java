be8e94a989bb685eec31d6275db2319d2de061fa
alexpet2002 <a.petukhova2002@gmail.com>
2022-05-09 01:40:55 +0300

    Added getters and setters. Deleted space.

--- src/com/aueb/models/Products.java
+++ src/com/aueb/models/Products.java
@@ -6,10 +6,47 @@ public class Products {
     int modelYear;
     String modelManufacturer;
     double modelPrice;
-
     public double discount;
 
+    public String getModelName() {
+        return modelName;
+    }
 
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public String getModelManufacturer() {
+        return modelManufacturer;
+    }
+
+    public void setModelManufacturer(String modelManufacturer) {
+        this.modelManufacturer = modelManufacturer;
+    }
+
+    public double getModelPrice() {
+        return modelPrice;
+    }
+
+    public void setModelPrice(double modelPrice) {
+        this.modelPrice = modelPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
 
     double finalPrice() {
         return modelPrice - ( modelPrice * discount);
