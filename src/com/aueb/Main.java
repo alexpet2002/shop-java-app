6900004f81629feb1bc32d0a2d15b028b0518a1e
alexpet2002 <a.petukhova2002@gmail.com>
2022-05-14 16:20:00 +0300

    Modified Main class added a message. Modified Main class added a second message.

--- src/com/aueb/Main.java
+++ src/com/aueb/Main.java
@@ -5,6 +5,8 @@ public class Main {
     // Beginning of the application
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hi to my world..");
         System.out.println("Welcome to my Program!");
+        System.out.println("It is nice to see you back again");
     }
 }
