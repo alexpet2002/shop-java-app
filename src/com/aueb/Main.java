8b17c945664c46866ca11430970ee11fc7a9ab3b
alexpet2002 <a.petukhova2002@gmail.com>
2022-05-14 15:56:41 +0300

    Modified Main class and added Hello world message. Modified Main class and added welcome message. Modified Main class added exclamation mark.

--- src/com/aueb/Main.java
+++ src/com/aueb/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     // Beginning of the application
     public static void main(String[] args) {
-
+        System.out.println("Hello World!");
+        System.out.println("Welcome to my Program!");
     }
 }
