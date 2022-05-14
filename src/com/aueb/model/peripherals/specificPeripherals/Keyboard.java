7c81d41210c57d5521a06a6ff8e094b790db8eba
alexpet2002 <a.petukhova2002@gmail.com>
2022-05-14 14:30:56 +0300

    Products.java removed space.Implemented enum class in Monitor.java and Keyboard.java.Implemented toString and setChoice methods in Monitor.java. Added a main method to Monitor.java to test the code. Solved conflicts due to rebase and moved code to classes Keyboard and Monitor.
    (cherry picked from commit 8666e9a96d5b75804c0b9fbe38c5c29586474e78)

--- src/com/aueb/model/peripherals/specificPeripherals/Keyboard.java
+++ src/com/aueb/model/peripherals/specificPeripherals/Keyboard.java
@@ -1,6 +1,21 @@
 package com.aueb.model.peripherals.specificPeripherals;
 
-import com.aueb.model.peripherals.ComputerPeripheral;
+public class Keyboard {
+    enum Connection {
+        WIRELESS, WIRED;
 
-public class Keyboard extends ComputerPeripheral {
+        public String getMemory() {
+
+            switch (this) {
+                case WIRELESS:
+                    return "Wireless";
+
+                case WIRED:
+                    return "Wired";
+
+                default:
+                    return null;
+            }
+        }
+    }
 }
