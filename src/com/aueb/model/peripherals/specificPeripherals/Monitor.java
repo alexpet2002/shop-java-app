8666e9a96d5b75804c0b9fbe38c5c29586474e78
alexpet2002 <a.petukhova2002@gmail.com>
2022-05-14 14:30:56 +0300

    Products.java removed space.Implemented enum class in Monitor.java and Keyboard.java.Implemented toString and setChoice methods in Monitor.java. Added a main method to Monitor.java to test the code. Solved conflicts due to rebase and moved code to classes Keyboard and Monitor.

--- src/com/aueb/model/peripherals/specificPeripherals/Monitor.java
+++ src/com/aueb/model/peripherals/specificPeripherals/Monitor.java
@@ -1,6 +1,77 @@
 package com.aueb.model.peripherals.specificPeripherals;
 
-import com.aueb.model.peripherals.ComputerPeripheral;
+public class Monitor {
 
-public class Monitor extends ComputerPeripheral {
+    int choice1 = 0;
+
+    void setChoice(int choice1) {
+        this.choice1 = choice1;
+    }
+
+    enum Memory {
+        MONITOR, PORTABLE_MONITOR, TV_MONITOR;
+
+        public String getMemory() {
+
+            switch (this) {
+                case MONITOR:
+                    return "monitor";
+
+                case PORTABLE_MONITOR:
+                    return "portable monitor";
+
+                case TV_MONITOR:
+                    return "Tv monitor";
+
+                default:
+                    return null;
+            }
+        }
+    }
+
+    enum Port {
+        DISPLAY_PORT, HDMI, USB_C;
+
+        public String getPort() {
+            switch (this) {
+                case DISPLAY_PORT:
+                    return "Display port";
+
+                case HDMI:
+                    return "HDMI";
+
+                case USB_C:
+                    return "USB_C";
+
+                default:
+                    return null;
+
+            }
+
+        }
+    }
+
+    public String toString() {
+        if (choice1 == 0) {
+            return "Type of memory is " + Memory.MONITOR.getMemory();
+        }
+        if (choice1 == 1) {
+            return "Type of memory is " + Memory.PORTABLE_MONITOR.getMemory();
+        }
+        if (choice1 == 2) {
+            return "Type of memory is " + Memory.TV_MONITOR.getMemory();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Monitor monitor = new Monitor();
+        monitor.setChoice(2);
+        System.out.println(monitor);
+
+
+    }
 }
+
+
+
