d7b306cb402b6699e057f06c543bc5cadc1db411
AikVant <60099696+AikVant@users.noreply.github.com>
2022-05-15 18:53:00 +0300

    Modified Monitor, Keyboard, Mouse, Printer, ComputerPeripheral, Shop. Added notes to Shop and create new objects from above classes. Simple correct a wrong number in Cpu.

--- src/com/aueb/model/Shop.java
+++ src/com/aueb/model/Shop.java
@@ -1,6 +1,10 @@
 package com.aueb.model;
 
 import com.aueb.model.components.specificComponents.*;
+import com.aueb.model.peripherals.specificPeripherals.Keyboard;
+import com.aueb.model.peripherals.specificPeripherals.Monitor;
+import com.aueb.model.peripherals.specificPeripherals.Mouse;
+import com.aueb.model.peripherals.specificPeripherals.Printer;
 
 import java.util.ArrayList;
 
@@ -8,15 +12,15 @@ public class Shop {
 
     static int numOfAvailableProducts = 0;
     //    double discount;
-    //    Product[] availableItems;
+    //    Product[] availableItems; // don't need it, because we created ArrayList instead
     //    int numOfAvailableProducts;
-    //    Sale[] soldItems;
-    //    Order[] orderedItems;
+    //    Sale[] soldItems; // must create with ArrayList too
+    //    Order[] orderedItems; // must create with ArrayList too
     //    int numOfItems;
     //    Order order;
     //    Sale sale;
 
-    Product product = new Product();
+    // Product product = new Product(); // possible initialize arrayList of products with code in test main
 
     private ArrayList<Product> products = new ArrayList<Product>();
 
@@ -55,12 +59,20 @@ public class Shop {
         Ram ram = new Ram();
         GraphicsCard card = new GraphicsCard();
         HardDrive drive = new HardDrive();
+        Monitor monitor = new Monitor();
+        Keyboard keyboard = new Keyboard();
+        Mouse mouse = new Mouse();
+        Printer printer = new Printer();
 
         shop.storeProduct(motherboard);
         shop.storeProduct(cpu);
         shop.storeProduct(ram);
         shop.storeProduct(card);
         shop.storeProduct(drive);
+        shop.storeProduct(monitor);
+        shop.storeProduct(keyboard);
+        shop.storeProduct(mouse);
+        shop.storeProduct(printer);
 
         shop.showProducts();
     }
--- src/com/aueb/model/peripherals/specificPeripherals/Printer.java
+++ src/com/aueb/model/peripherals/specificPeripherals/Printer.java
@@ -2,5 +2,83 @@ package com.aueb.model.peripherals.specificPeripherals;
 
 import com.aueb.model.peripherals.ComputerPeripheral;
 
+import java.util.Scanner;
+
 public class Printer extends ComputerPeripheral {
+    private static String printerTech;
+    static final String LASER = "Laser";
+    static final String INKJET = "Inkjet";
+    private static String printType;
+    static final String COLORED = "Colored";
+    static final String BLACK_WHITE = "Black and White";
+    private static int numOfPrinters = 0;
+
+    public Printer() {
+        printerTech = "";
+        printType = "";
+        numOfPrinters++;
+    }
+
+    static void setPrinterTech(String Type) {
+        if (Type.equals("1"))
+            printerTech = LASER;
+        else
+            printerTech = INKJET;
+    }
+
+    static void setPrintType(String Type) {
+        if (Type.equals("1"))
+            printType = COLORED;
+        else
+            printType = BLACK_WHITE;
+    }
+
+    String getPrinterTech() {
+        return printerTech;
+    }
+
+    String getPrintType() {
+        return printType;
+    }
+
+    public String toString() {
+        return "\nPrinter Technology: " + getPrinterTech() + "\n"
+                + "Printing Type: " + getPrintType() + "\n";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("9. Select Printer: ");
+        System.out.print("Choice? ");
+
+        String select = in.nextLine();
+        if (select.equals("9")) {
+            Printer printer = new Printer();
+            System.out.println("Set the features of the printer");
+
+            System.out.println("Printer technology:");
+            System.out.println("1. Laser");
+            System.out.println("2. Inkjet");
+            System.out.print("Choice? ");
+            select = in.nextLine();
+            if (select.equals("1"))
+                setPrinterTech("1");
+            else
+                setPrinterTech("2");
+
+            System.out.println("Mouse connection:");
+            System.out.println("1. Wired");
+            System.out.println("2. Wireless");
+            System.out.print("Choice? ");
+            select = in.nextLine();
+            if (select.equals("1"))
+                setPrintType("1");
+            else
+                setPrintType("2");
+
+            System.out.println(printer);
+        }
+    }
+
 }
