14fe6411ec88693edb20770c86dccdf9e6c5f230
alexpet2002 <a.petukhova2002@gmail.com>
2022-05-28 13:09:16 +0300

    Removed all unused methods.Added our names. Added empty constructors. Added comments to main

--- ComputerComponent.java
+++ ComputerComponent.java
@@ -1,14 +1,23 @@
+/*
+    Names: ALEKSANDRA PETUKHOVA, AIKATERINI VANTARAKI
+    Student Numbers: 3210229, 3210020
+    Team number: 011
 
+*/
 public abstract class ComputerComponent extends Product {
     // Component discount
     public static final double COMPONENT_DISCOUNT = 0.25;
 
+    //Constructors for ComputerComponent
     protected ComputerComponent(String modelName) {
         super(modelName);
     }
 
     protected ComputerComponent(String modelName, int modelYear, String modelManufacturer, double modelPrice) {
         super(modelName, modelYear, modelManufacturer, modelPrice, COMPONENT_DISCOUNT);
+    }
+
+    public ComputerComponent() {
     }
 
     //toString method for Component
